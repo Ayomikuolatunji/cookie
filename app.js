1d18f7e3d0c94c45351344246744cd2989785f0e
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-03 21:19:20 +0100

    connect to the database

--- app.js
+++ app.js
@@ -34,7 +34,10 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://mongoose:john123@cluster0.xcjno.mongodb.net/myMongooseDatabase?retryWrites=true&w=majority',{
+         useNewUrlParser: true,
+         useUnifiedTopology: true 
+  }
   )
   .then(result => {
     User.findOne().then(user => {
