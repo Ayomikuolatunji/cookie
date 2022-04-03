7de6c9e777799da6ba7b330a54b9445af4898904
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-03 23:15:27 +0100

    auth cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,19 @@
 
 exports.getLogin= (req, res, next) => {
-    const isLoggedIn=req.get("Cookie")
-    .split(";")[1]
-    .trim()
-    .split("=")[1]
-    console.log(isLoggedIn)
+    // const isLoggedIn=req.get("Cookie")
+    // .split(";")[1]
+    // .trim()
+    // .split("=")[1]
+    // console.log(isLoggedIn)
     res.render('auth/login', {
         path: '/orders',
         pageTitle: 'Login Page',
-        isAuthenticated:isLoggedIn
+        isAuthenticated:false
     })
 };
 
   
 exports.postLogin=(req,res,next)=>{
-    res.setHeader("Set-Cookie", "isLoggedIn=true")
+    res.setHeader("Set-Cookie", "isLoggedIn=true; Secure")
     res.redirect("/")
 }
